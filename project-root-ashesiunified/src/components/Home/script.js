1445bfcca10cf3dabe25fd281642bd513523cb2d
kwakuNti <159504184+kwakuNti@users.noreply.github.com>
2024-03-07 17:32:12 +0000

    Delete project-root-ashesiunified/src directory

--- project-root-ashesiunified/src/components/Home/script.js
+++
@@ -1,3 +0,0 @@
- document.getElementById("getStartedButton").addEventListener("click", function() {
-    window.location.href = "register.html"; // Change "register_page.php" to the actual URL of your register page
-});
--- project-root-ashesiunified/src/pages/Home/js/script.js
+++
@@ -1,3 +0,0 @@
- document.getElementById("getStartedButton").addEventListener("click", function() {
-    window.location.href = "../Register/Register.html";
-});
